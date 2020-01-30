8635ccdbacd891b6f2434573e4807bf4d8921339
AlexSalazarJ <Alex@Alejandros-MacBook-Pro.local>
2020-01-30 13:55:08 -0800

    added  javascript so the contact form is hidden

---
+++ 3-portfolio.js
@@ -0,0 +1,10 @@
+let request = document.querySelector("button");
+let form = document.querySelector("form");
+
+request.addEventListener("click", () => {
+  if (form.style.display === "none") {
+    form.style.display = "block";
+  } else {
+    form.style.display = "none";
+  }
+});
