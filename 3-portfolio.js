9bf180f7a6fbbee287fc8e0aa88d63bef7082992
AlexSalazarJ <Alex@Alejandros-MacBook-Pro.local>
2020-01-30 21:45:22 -0800

    updating about me

--- 3-portfolio.js
+++ 3-portfolio.js
@@ -10,10 +10,15 @@ function requestSent() {
   form.style.display = "none";
   document.getElementById("request").style.display = "block";
   document.getElementById("sent").style.display = "flex";
-  return false;
+  clearscreen();
 }
 function clearscreen() {
   document.getElementById("name").value = "";
   document.getElementById("email").value = "";
   document.getElementById("comment").value = "";
+}
+function cancel() {
+  clearscreen();
+  form.style.display = "none";
+  document.getElementById("request").style.display = "block";
 }
