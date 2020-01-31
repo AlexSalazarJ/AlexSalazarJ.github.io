361acf16a64448a8c34e95f73060375b5a523eed
AlexSalazarJ <Alex@Alejandros-MacBook-Pro.local>
2020-01-30 20:56:23 -0800

    fixed bug with the form

--- 3-portfolio.js
+++ 3-portfolio.js
@@ -3,10 +3,17 @@ let form = document.querySelector("form");
 function requestForm() {
   form.style.display = "block";
   document.getElementById("request").style.display = "none";
+  document.getElementById("sent").style.display = "none";
 }
 
 function requestSent() {
   form.style.display = "none";
   document.getElementById("request").style.display = "block";
+  document.getElementById("sent").style.display = "flex";
+  return false;
 }
-function clearForm() {}
+function clearscreen() {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("comment").value = "";
+}
