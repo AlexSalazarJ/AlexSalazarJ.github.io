66df52a91bad8c5c9dcdf906edf902e064b28cd8
Alex <alejan5@pdx.edu>
2021-08-16 20:22:58 -0700

    updated dependency

--- javascript/index.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   var elems = document.querySelectorAll('select');
-//   var instances = M.FormSelect.init(elems);
-// });
-$(document).ready(function () {
-  $('select').formSelect();
-  // Old way
-  // $('select').material_select();
-});
