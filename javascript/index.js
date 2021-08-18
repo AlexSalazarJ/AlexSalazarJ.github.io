646fbd9d2352c981336c967aa16c17676869956c
Alex <alejan5@pdx.edu>
2021-08-17 20:14:15 -0700

    added basic pages for all required items

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
   M.updateTextFields();
 });
+function requestForm() {
+  document.getElementById('contact-form').className = 'scale-in';
+}
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
